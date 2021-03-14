57f3f0af4cc0aeb9461a8687c95770b37f95bad6
aprilahijriyan <hijriyan23@gmail.com>
2021-03-14 15:06:08 +0700

    🎉 Initial Commit

---
+++ src/pages/Login.vue
@@ -0,0 +1,31 @@
+<template>
+  <q-page padding class="window-height window-width row justify-center items-center">
+    <!-- content -->
+    <div class="q-pa-md">
+      <div class="column q-gutter-md " style="max-width: 500px;">
+        <q-input type="email" v-model="text" label="Email">
+          <template v-slot:prepend>
+            <q-icon name="mail" />
+          </template>
+        </q-input>
+
+        <q-input type="password" v-model="text" label="Password">
+          <template v-slot:prepend>
+            <q-icon name="lock" />
+          </template>
+        </q-input>
+      </div>
+  </div>
+  </q-page>
+</template>
+
+<script>
+export default {
+  // name: 'PageName',
+  data() {
+    return {
+      text: ""
+    }
+  }
+}
+</script>
