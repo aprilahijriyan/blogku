0a20b7d2ff5cade444fed36024b38b4129fc3108
aprilahijriyan <hijriyan23@gmail.com>
2021-03-17 04:03:52 +0700

    👌 IMPROVE: Mengganti posisi tanggal artikel

--- src/components/Artikel.vue
+++ src/components/Artikel.vue
@@ -2,17 +2,22 @@
   <q-item clickable v-ripple @click="onArticleClick">
     <div class="row">
       <div :class="'col-md-' + (isPopuler ? 5 : 4)">
-        <q-item-section thumbnail>
+        <q-item-section thumbnail class="relative-position">
           <img style="border-radius: 8px" class="fit" src="https://cdn.quasar.dev/img/mountains.jpg">
+          <div class="topright"><q-btn round color="white" text-color="primary" icon="bookmark" :size="isPopuler ? 'sm' : ''"/></div>
         </q-item-section>
       </div>
       <div class="col">
         <div class="lt-md"><div class="q-mt-sm"></div></div>
-        <q-item-section>
-          <q-item-label class="text-bold text-capitalize" :style="'font-size: ' + (isPopuler ? '16px' : '22px')">Title goes here</q-item-label>
-        </q-item-section>
-        <q-item-label caption :lines="isPopuler ? 3 : 6" :style="isPopuler ? '': 'font-size: 16px'">
-          <span class="text-weight-bold">Janet</span>
+        <div class="flex justify-between q-mb-sm">
+          <q-item-section>
+            <q-item-label :class="'text-bold text-capitalize ' + ($q.platform.is.desktop ? 'ellipsis' : '')" :style="'font-size: ' + (isPopuler ? '16px' : '22px')">Membuat REST API dengan zemfrog framework!</q-item-label>
+          </q-item-section>
+        </div>
+        <q-item-label caption :lines="isPopuler ? 3 : 5" :style="isPopuler ? '': 'font-size: 16px'">
+          <div class="row">
+            <p>By <span class="text-weight-bold">Aprila Hijriyan - 5 min ago</span></p>
+          </div>
           -- I'll be in your neighborhood doing errands this
           weekend. Do you want to grab brunch?
           -- I'll be in your neighborhood doing errands this
