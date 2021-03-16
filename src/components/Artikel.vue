c7cc6a18522e6ecf07a6b74a538ce6dd3fa37d9d
aprilahijriyan <hijriyan23@gmail.com>
2021-03-16 22:58:42 +0700

    💥 NEW: add landing page (wip)

---
+++ src/components/Artikel.vue
@@ -0,0 +1,58 @@
+<template>
+  <q-item clickable v-ripple @click="onArticleClick">
+    <div class="row">
+      <div :class="'col-md-' + (isPopuler ? 5 : 4)">
+        <q-item-section thumbnail>
+          <img style="border-radius: 8px" class="fit" src="https://cdn.quasar.dev/img/mountains.jpg">
+        </q-item-section>
+      </div>
+      <div class="col">
+        <div class="lt-md"><div class="q-mt-sm"></div></div>
+        <q-item-section>
+          <q-item-label class="text-bold text-capitalize" :style="'font-size: ' + (isPopuler ? '16px' : '22px')">Title goes here</q-item-label>
+        </q-item-section>
+        <q-item-label caption :lines="isPopuler ? 3 : 6" :style="isPopuler ? '': 'font-size: 16px'">
+          <span class="text-weight-bold">Janet</span>
+          -- I'll be in your neighborhood doing errands this
+          weekend. Do you want to grab brunch?
+          -- I'll be in your neighborhood doing errands this
+          weekend. Do you want to grab brunch?
+          -- I'll be in your neighborhood doing errands this
+          weekend. Do you want to grab brunch?
+          -- I'll be in your neighborhood doing errands this
+          weekend. Do you want to grab brunch?
+          -- I'll be in your neighborhood doing errands this
+          weekend. Do you want to grab brunch?
+          -- I'll be in your neighborhood doing errands this
+          weekend. Do you want to grab brunch?
+          -- I'll be in your neighborhood doing errands this
+          weekend. Do you want to grab brunch?
+        </q-item-label>
+        <div class="flex q-mt-sm">
+          <q-chip class="text-bold text-capitalize" clickable @click="onClick" :size="isPopuler? 'sm': 'md'">tag</q-chip>
+        </div>
+      </div>
+    </div>
+  </q-item>
+  
+</template>
+
+<script>
+export default {
+  // name: 'ComponentName',
+  data () {
+    return {}
+  },
+  props: {
+    isPopuler: Boolean
+  },
+  methods: {
+    onArticleClick() {
+      console.log("article clicked")
+    },
+    onClick() {
+      console.log("chip clicked")
+    }
+  }
+}
+</script>
--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -1,14 +1,78 @@
 <template>
-  <q-page class="flex flex-center">
-    <img
-      alt="Quasar logo"
-      src="~assets/quasar-logo-full.svg"
+  <q-page>
+    <q-carousel
+      animated
+      v-model="slide"
+      arrows
+      navigation
+      infinite
     >
+      <q-carousel-slide :name="1" img-src="https://cdn.quasar.dev/img/mountains.jpg" />
+      <q-carousel-slide :name="2" img-src="https://cdn.quasar.dev/img/parallax1.jpg" />
+      <q-carousel-slide :name="3" img-src="https://cdn.quasar.dev/img/parallax2.jpg" />
+      <q-carousel-slide :name="4" img-src="https://cdn.quasar.dev/img/quasar.jpg" />
+    </q-carousel>
+    <div class="q-pa-md q-gutter-md">
+      <div class="row flex justify-between">
+        <div class="col-md">
+          <div class="row q-gutter-sm">
+            <q-input outlined v-model="text" label="Cari apa?" class="col">
+              <template v-slot:prepend>
+                <q-icon name="search" />
+              </template>
+              <template v-slot:append v-if="text">
+                <q-icon name="close" @click="text = ''" class="cursor-pointer" />
+              </template>
+            </q-input>
+            <q-btn icon="filter_list"/>
+          </div>
+          <div class="flex q-gutter-sm q-mt-sm text-capitalize">
+            <q-chip v-for="i in ($q.platform.is.desktop ? 8 : 3)" :key="i" clickable @click="onClick" text-color="white" color="primary" icon="label">
+              tag
+            </q-chip>
+            <q-chip outline clickable @click="onClick" class="text-bold text-capitalize">lihat semua</q-chip>
+          </div>
+          <q-list separator bordered class="q-mt-md">
+              <artikel v-for="i in 5" :key="i" />
+            </q-list>
+          <div class="lt-md q-mb-lg"></div>
+        </div>
+        <div class="gt-sm">
+          <div class="q-mr-sm"></div>
+        </div>
+        <div class="col-md-4">
+          <q-card>
+            <q-card-section>
+              <div class="flex justify-between">
+                <div class="text-h5">Populer</div>
+                <a href="#" class="text-bold no-underline text-blue" @click.prevent="onClick">Lihat semua</a>
+              </div>
+            </q-card-section>
+            <q-list separator bordered >
+              <artikel v-for="i in 5" :key="i" :isPopuler="true"/>
+            </q-list>
+          </q-card>
+        </div>
+      </div>
+    </div>
   </q-page>
 </template>
 
 <script>
+import Artikel from 'src/components/Artikel.vue'
 export default {
-  name: 'PageIndex'
+  components: { Artikel },
+  name: 'PageIndex',
+  data() {
+    return {
+      slide: 1,
+      text: ""
+    }
+  },
+  methods: {
+    onClick() {
+      console.log("chip clicked")
+    }
+  }
 }
 </script>
