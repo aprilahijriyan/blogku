14848475f3987311b0bba61b274bf9294d4d21e1
aprilahijriyan <hijriyan23@gmail.com>
2021-03-19 22:28:52 +0700

    🐛 FIX: bungkus komponen dengan anchor tag

--- src/components/ArtikelTerbaru.vue
+++ src/components/ArtikelTerbaru.vue
@@ -1,10 +1,12 @@
 <template>
-  <q-img class="fit" src="https://cdn.quasar.dev/img/mountains.jpg" style="z-index: 0; position: absolute; left: 0px; top: 0px; opacity: 0.8; ">
-    <div class="absolute-bottom text-subtitle1">
-      <div :class="'text-bold text-capitalize ' + ($q.platform.is.desktop ? 'ellipsis' : '')" :style="'font-size: ' + ($q.platform.is.mobile ? '16px' : '22px')">Membuat REST API dengan zemfrog framework</div>
-      <p>By <span class="text-weight-bold">Aprila Hijriyan - 5 min ago</span></p>
-    </div>
-  </q-img>
+  <a href="#">
+    <q-img class="fit" src="https://cdn.quasar.dev/img/mountains.jpg" style="z-index: 0; position: absolute; left: 0px; top: 0px; opacity: 0.8; ">
+      <div class="absolute-bottom text-subtitle1">
+        <div :class="'text-bold text-capitalize ' + ($q.platform.is.desktop ? 'ellipsis' : '')" :style="'font-size: ' + ($q.platform.is.mobile ? '16px' : '22px')">Membuat REST API dengan zemfrog framework</div>
+        <p>By <span class="text-weight-bold">Aprila Hijriyan - 5 min ago</span></p>
+      </div>
+    </q-img>
+  </a>
 </template>
 
 <script>
