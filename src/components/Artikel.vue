80fa25b6b1a91bb057d224a8782772226895b511
aprilahijriyan <hijriyan23@gmail.com>
2021-03-19 22:24:56 +0700

    👌 IMPROVE: remove bookmark button & configure lines

--- src/components/Artikel.vue
+++ src/components/Artikel.vue
@@ -2,9 +2,8 @@
   <q-item clickable v-ripple @click="onArticleClick">
     <div class="row">
       <div :class="'col-md-' + (isPopuler ? 5 : 4)">
-        <q-item-section thumbnail class="relative-position">
+        <q-item-section thumbnail>
           <img style="border-radius: 8px" class="fit" src="https://cdn.quasar.dev/img/mountains.jpg">
-          <div class="topright"><q-btn round color="white" text-color="primary" icon="bookmark" :size="isPopuler ? 'sm' : ''"/></div>
         </q-item-section>
       </div>
       <div class="col">
@@ -14,11 +13,12 @@
             <q-item-label :class="'text-bold text-capitalize ' + ($q.platform.is.desktop ? 'ellipsis' : '')" :style="'font-size: ' + (isPopuler ? '16px' : '22px')">Membuat REST API dengan zemfrog framework!</q-item-label>
           </q-item-section>
         </div>
-        <q-item-label caption :lines="isPopuler ? 3 : 5" :style="isPopuler ? '': 'font-size: 16px'">
+        <q-item-label caption :lines="isPopuler ? 2 : 5" :style="isPopuler ? '': 'font-size: 16px'">
           <div class="row">
             <p>By <span class="text-weight-bold">Aprila Hijriyan - 5 min ago</span></p>
           </div>
-          -- I'll be in your neighborhood doing errands this
+          <div class="text-bold">
+            -- I'll be in your neighborhood doing errands this
           weekend. Do you want to grab brunch?
           -- I'll be in your neighborhood doing errands this
           weekend. Do you want to grab brunch?
@@ -32,6 +32,7 @@
           weekend. Do you want to grab brunch?
           -- I'll be in your neighborhood doing errands this
           weekend. Do you want to grab brunch?
+          </div>
         </q-item-label>
         <div class="flex q-mt-sm">
           <q-chip class="text-bold text-capitalize" clickable @click="onClick" :size="isPopuler? 'sm': 'md'">tag</q-chip>
