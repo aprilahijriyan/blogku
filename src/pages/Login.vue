08fd3633b71855f21309faaae174a2df45984d6b
aprilahijriyan <hijriyan23@gmail.com>
2021-03-28 01:24:12 +0700

    👌 IMPROVE: login logic

--- src/pages/Login.vue
+++ src/pages/Login.vue
@@ -1,28 +1,30 @@
 <template>
-  <q-page padding class="window-height window-width row justify-center items-center bg-teal-4">
+  <q-page padding class="window-height window-width row justify-center items-center">
     <!-- content -->
-    <div class="q-gutter-md" style="width: 350px;">
+    <form @submit="onSubmit">
+      <div class="q-gutter-md" style="width: 350px;">
       <div class="column q-gutter-md q-pa-md shadow-10 bg-white" style="border-radius: 5px">
-        <q-input type="email" v-model="text" label="Email">
+        <q-input type="email" v-model="email" label="Email">
           <template v-slot:prepend>
             <q-icon name="mail" />
           </template>
         </q-input>
 
-        <q-input type="password" v-model="text" label="Password">
+        <q-input type="password" v-model="password" label="Password">
           <template v-slot:prepend>
             <q-icon name="lock" />
           </template>
         </q-input>
         <div class="flex justify-end">
-          <a href="#">Lupa Password?</a>
+          <a href="#" @click.prevent="$router.push('/request-password-reset')">Lupa Password?</a>
         </div>
-        <q-btn push color="green-4" text-color="white" label="LOGIN" />
+        <q-btn push type="submit" color="green-4" text-color="white" label="LOGIN" />
       </div>
       <div class="flex justify-center q-gutter-y-md shadow-3 bg-white" style="border-radius: 2px">
-        <p>Baru di blogku ? <a href="#">Buat akun</a></p>
+        <p>Baru di blogku ? <a href="#" @click.prevent="$router.push('/register')">Buat akun</a></p>
       </div>
     </div>
+    </form>
   </q-page>
 </template>
 
@@ -31,7 +33,21 @@ export default {
   // name: 'PageName',
   data() {
     return {
-      text: ""
+      email: "",
+      password: ""
+    }
+  },
+  methods: {
+    async onSubmit(evt) {
+      console.log("login... ")
+      let success = await this.$store.dispatch(
+        'auth/login',
+        {
+          email: this.email,
+          password: this.password
+        }
+      )
+      console.log("success: " + success)
     }
   }
 }
